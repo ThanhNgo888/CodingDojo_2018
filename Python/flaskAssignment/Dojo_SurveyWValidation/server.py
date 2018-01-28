9fcd786acec956d1e2c1241c4aeeff2421baa5b3
Thanh Ngo <thanhngo26@hotmail.com>
2018-01-28 14:34:52 -0800

    updated

--- Python/flaskAssignment/Dojo_SurveyWValidation/server.py
+++ Python/flaskAssignment/Dojo_SurveyWValidation/server.py
@@ -1,36 +1,36 @@
 from flask import Flask, render_template, request, redirect, session, flash
+#initialize the Flask application
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
 
 # our index route will handle rendering our form
-@app.route('/',methods=['GET']) # methods= ['Get'] (default)
+@app.route('/',methods=['GET', 'POST'])
 def index():
-  return render_template("index.html")
-# this route will handle our form submission
+    return render_template('index.html')
 
 @app.route('/process',methods=['POST'])
-def processForm():
+def process():
     if len(request.form['name']) < 1:
-      flash("Name cannot be empty!") # just pass a string to the flash function
-    elif request.form['comment'] == "":
-      flash("Comment box cannot be empty!") # just pass a string to the flash function
+       flash("Name cannot be empty. Please try again!")
+       return redirect('/')
+
+    elif len(request.form['comment']) < 10:
+       flash("Comment cannot be empty or less than 120 characters. Please try again!")
+       return redirect('/')
     else:
-      flash("Success! Your name is {}".format(request.form['name'])) # just pass a string to the flash function
-      return redirect('/')
+        flash("Success! Your name is {}".format(request.form['name']))
+
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
     session['comment'] = request.form['comment']
-    comment = countLetters(session['comment'])
-    print comment
-    if comment > 120:
-        flash('Comment cannot be more than 120 words. Please try again!')
-        return redirect('/')
-
-    #return render_template("result.html",
-    #     # name='Michael Choi',
-    #     # location='San Jose',
-    #     # language='Python!!!',
-    #     # comment='Hi there :)')
-    #
-
-
+    return redirect('/show')
+@app.route('/show')
+def show():
+    return render_template('result.html')
+    return redirect('/')
+@app.route("/back", methods=['POST'])
+def goBack():
+    return redirect('/')
 if __name__ == '__main__':
     app.run(debug=True) # run our server
