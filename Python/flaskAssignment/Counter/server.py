3e2573a6147a3b53be215605f30b8b0254d52db2
Thanh Ngo <thanhngo26@hotmail.com>
2018-01-27 17:28:43 -0800

    First Commit

--- Python/flaskAssignment/Counter/server.py
+++ Python/flaskAssignment/Counter/server.py
@@ -4,21 +4,30 @@ app.secret_key = 'ThisIsSecret' # you need to set a secret key for security purp
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    if 'count' not in session:
+        session["count"] = 0
+    session["count"] += 1
     return render_template ("index.html")
-@app.route('/users', methods=['POST'])
-def create_user():
-   # here we add two properties to session to store the name and email
-   session['name'] = request.form['name']
-   session['email'] = request.form['email']
-   return redirect('/show') # noticed that we changed where we redirect to so that we can go to the page that displays the name and email!
-@app.route('/show')
-def show_user():
-    return render_template('counter.html')
 
-  counter = 1
-  counter +=1
-  return str(counter)
+@app.route('/count', methods=['POST'])
+def displayCount():
+    if 'count' not in session:
+        session["count"] = 0
+    session["count"] = 1
+    return redirect('/')
 
+@app.route('/ninja', methods=['POST'])
+def ninja():
+    session['ninja'] = request.form['ninja']
+    if 'count' not in session:
+        session["count"] = 0
+    session["count"] += 1
+    return redirect('/')
 
+@app.route('/hackers', methods=['POST'])
+def hackers():
+    session['hackers'] = request.form['hackers']
+    session["count"] = 0
+    return redirect('/')
 if __name__ == '__main__':
     app.run(debug=True) # run our server
